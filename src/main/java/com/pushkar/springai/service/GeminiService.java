492d209402f4e08a5f1f4104db36d1d9d07ed771
iampushkar <m4manishpushkar@gmail.com>
2026-06-14 00:10:15 +0530

    refactoring the code

--- src/main/java/com/pushkar/springai/service/GeminiService.java
+++ src/main/java/com/pushkar/springai/service/GeminiService.java
@@ -8,12 +8,14 @@ public class GeminiService {
 
     private final ChatClient chatClient;
 
-    public GeminiService(ChatClient chatClient) {
-        this.chatClient = chatClient;
+    public GeminiService(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
     }
 
     public String getGeminiResponse(String query) {
-        String response = chatClient.prompt().user(query).call().content();
+        String response = chatClient.prompt(query)
+                .call()
+                .content();
         System.out.println("Gemini response: " + response);
         return response;
     }
